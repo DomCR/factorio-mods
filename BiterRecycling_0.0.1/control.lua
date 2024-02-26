1da7e25c99eaed018bfe62d873680b295c65c85f
DomCR <DomCR@users.noreply.github.com>
2024-02-26 21:39:19 +0100

    science

--- BiterRecycling_0.0.1/control.lua
+++ BiterRecycling_0.0.1/control.lua
@@ -1,24 +1,47 @@
 local function on_entity_died_drop(event)
-	local min = 0
-	local max = 1 + event.entity.pollution_bonus
+	if
+		event.entity.type == "unit-spawner" or
+		event.entity.type == "turret"
+	then
+		local drop_type = math.random(0, 6)
+		local item_count = math.floor(math.random(5, 10) + event.entity.pollution_bonus * 0.1)
+		local item_name
 
+		if drop_type == 0 then
+			item_name = "automation-science-pack"
+		elseif (drop_type == 1) then
+			item_name = "chemical-science-pack"
+		elseif (drop_type == 2) then
+			item_name = "military-science-pack"
+		elseif (drop_type == 3) then
+			item_name = "chemical-science-pack"
+		elseif (drop_type == 4) then
+			item_name = "production-science-pack"
+		elseif (drop_type == 5) then
+			item_name = "logistic-science-pack"
+		elseif (drop_type == 6) then
+			item_name = "utility-science-pack"
+		end
 
-	local drop_type = math.random(0, 4)
-	local item_count = math.random() + event.entity.pollution_bonus * 0.1
+		event.loot.insert { name = item_name, probability = 0.5, count = item_count }
+	else
+		local drop_type = math.random(0, 4)
+		local item_count = math.floor(math.random(0, 1) + event.entity.pollution_bonus * 0.1)
 
-	if (drop_type == 1) then
-		event.loot.insert { name = "iron-ore", probability = 0.5, count = item_count }
-	elseif (drop_type == 2) then
-		event.loot.insert { name = "copper-ore", probability = 0.5, count = item_count }
-	elseif (drop_type == 3) then
-		event.loot.insert { name = "coal", probability = 0.5, count = item_count }
-	elseif (drop_type == 4) then
-		event.loot.insert { name = "stone", probability = 0.5, count = item_count }
-	elseif (drop_type == 5) then
-		event.loot.insert { name = "uranium-ore", probability = 0.5, count = item_count }
+		if item_count == 0 then
+		elseif (drop_type == 1) then
+			event.loot.insert { name = "iron-ore", probability = 0.5, count = item_count }
+		elseif (drop_type == 2) then
+			event.loot.insert { name = "copper-ore", probability = 0.5, count = item_count }
+		elseif (drop_type == 3) then
+			event.loot.insert { name = "coal", probability = 0.5, count = item_count }
+		elseif (drop_type == 4) then
+			event.loot.insert { name = "stone", probability = 0.5, count = item_count }
+		elseif (drop_type == 5) then
+			event.loot.insert { name = "uranium-ore", probability = 0.5, count = item_count }
+		end
 	end
-
-	-- local cooper = event.loot.insert { name = "copper-ore", probability = 0.5, count_min = min, count_max = max }
+	
 end
 
 script.on_event(
